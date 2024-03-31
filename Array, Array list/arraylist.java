36f4fcb421743591d7d6aa829e027ff7c1f91aaf
123Anurag123 <159927017+123Anurag123@users.noreply.github.com>
2024-03-31 09:32:31 +0530

    Add files via upload

---
+++ Array, Array list/arraylist.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args) {
+       
+ArrayList<String> aa= new ArrayList<>();
+//System.out.println(aa.size());
+//add
+aa.add("A");
+aa.add("A");
+aa.add("A");
+aa.add("A");
+aa.add("A");
+aa.add("A");
+aa.add("A");
+aa.add("A");
+System.out.println(aa);
+
+        
+
+        
+        
+    }
+}
